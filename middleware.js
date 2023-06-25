d992aa8b67d2583eecc9437515160caabbec8cfc
saswat <saswatranjan0602@gmail.com>
2023-06-25 18:58:30 +0530

    add star rating

--- middleware.js
+++ middleware.js
@@ -5,9 +5,17 @@ const Review = require("./models/review");
 
 
 
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
+  }
+  next();
+};
+
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl;
+    req.session.returnTo = req.originalUrl; // add this line
     req.flash("error", "You must be signed in first!");
     return res.redirect("/login");
   }
