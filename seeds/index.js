fdb811ad017f2a5272d865b7ec4009941ae9cebb
saswat <saswatranjan0602@gmail.com>
2023-06-28 10:16:10 +0530

    add map

--- seeds/index.js
+++ seeds/index.js
@@ -19,18 +19,29 @@ db.once("open", () => {
 const sample = array =>array[Math.floor(Math.random()*array.length)];
 
 
+
+
+
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      // USER ID
       author: "649274a19e39edff1c174e92",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
@@ -46,6 +57,6 @@ const seedDB = async () => {
   }
 };
 
-seedDB().then(()=>{
-  mongoose.connection.close()
-})
+seedDB().then(() => {
+  mongoose.connection.close();
+});
